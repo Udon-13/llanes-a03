3cd949ecad338070197bd688c983b37f4ed99fa1
Udon-13 <maxllanes@gmail.com>
2021-10-01 16:18:38 -0400

    psuedocode added for ex24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,54 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Max Llanes
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution24 {
+    /*
+        method: charCheck (int limit int currentCheck, String word1, String word2)
+            for (i; 0 to limit; step)
+                if word1[i] == word2[i]
+                    bool check = true
+            if check = false
+                false output statement
+                exit
+     */
+
+    /*
+        method: sortStrings(String str1)
+            make str1 char array
+            use prebuilt sorting array method
+            return str1
+     */
+
+    /*
+        method: lengthCheck (String str1 String str2)
+            if str1 length == str2 length
+                return str length
+            else
+                false statement
+                exit
+     */
+
+    /*
+        method: isAnagram (String str1 String str20
+            limit = lengthCheck
+            str1 = sortStrings
+            str2 = sortStrings
+            charCheck
+            output true
+
+     */
+    public static void main(String[] args) {
+        /*
+            prompt for two strings
+            check = isAnagram
+            if check
+                true statement
+         */
+    }
+}
