0bed68791662498dcfdf1c48688408ed44dc1a43
Udon-13 <maxllanes@gmail.com>
2021-10-02 15:20:41 -0400

    added code

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+a
+1exercise24/src/main/java/baseline/Solution24.java,f\a\fad1ea6bdd615bb50555d22352bfe5201a71226d
--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -6,6 +6,8 @@
 package baseline;
 
 import java.util.Scanner;
+import java.util.Arrays;
+import java.lang.String;
 
 public class Solution24 {
     /*
@@ -17,6 +19,29 @@ public class Solution24 {
                 false output statement
                 exit
      */
+    static void falseStatement(int check)
+    {
+        switch (check)
+        {
+            case 1:
+                System.out.println("Your words aren't the same size.");
+            case 2:
+                System.out.println("Your words aren't anagrams.");
+        }
+    }
+
+    static void charCheck (int limit, char word1[], char word2[])
+    {
+        for (int i=0; i<limit; i++)
+        {
+            if(word1[i] != word2[i])
+            {
+                falseStatement(2);
+                System.exit(0);
+            }
+        }
+    }
+
 
     /*
         method: sortStrings(String str1)
@@ -24,6 +49,13 @@ public class Solution24 {
             use prebuilt sorting array method
             return str1
      */
+
+    static char[] sortStrings(String str)
+    {
+        char sortedArray[] = str.toCharArray();
+        Arrays.sort(sortedArray);
+        return sortedArray;
+    }
 
     /*
         method: lengthCheck (String str1 String str2)
@@ -34,6 +66,20 @@ public class Solution24 {
                 exit
      */
 
+    static int lengthCheck (String str1, String str2)
+    {
+        if(str1.length() == str2.length() )
+        {
+            return str1.length();
+        }
+        else
+        {
+            falseStatement(1);
+            System.exit(0);
+        }
+        return 0;
+    }
+
     /*
         method: isAnagram (String str1 String str20
             limit = lengthCheck
@@ -43,6 +89,16 @@ public class Solution24 {
             output true
 
      */
+
+    static boolean isAnagram (String str1, String str2)
+    {
+        int limit = lengthCheck(str1, str2);
+        char array1[] = sortStrings(str1);
+        char array2[] = sortStrings(str2);
+        charCheck(limit, array1, array2);
+        return true;
+    }
+
     public static void main(String[] args) {
         /*
             prompt for two strings
@@ -50,5 +106,18 @@ public class Solution24 {
             if check
                 true statement
          */
+        Scanner input = new Scanner(System.in);
+        String str1, str2;
+        boolean check =false;
+        System.out.print("Type your first word: ");
+        str1 = input.nextLine();
+        System.out.print("Type your second word: ");
+        str2 = input.nextLine();
+
+         check = isAnagram(str1,str2);
+         if(check)
+         {
+             System.out.println("\"" + str1 + "\"" + " and " + "\"" + str2 + "\" are anagrams." );
+         }
     }
 }
