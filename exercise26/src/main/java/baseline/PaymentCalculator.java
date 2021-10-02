4898889981ed86b54db5ec4901fb8d7da583882e
Udon-13 <maxllanes@gmail.com>
2021-10-02 18:58:09 -0400

    added pseudocode for ex26

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,9 @@
+package baseline;
+
+public class PaymentCalculator {
+    /*
+    prompt for balance, APR, monthPay
+    use formula to get months
+    return months rounded up
+     */
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class Solution26 {
+    public static void main(String[] args) {
+        /*
+            int month = call method from paymentCalc class
+            output the months
+         */
+    }
+}
