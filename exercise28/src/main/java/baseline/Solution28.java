b76dacb2e3df31981a551e5418eb972e18cdd58d
Udon-13 <maxllanes@gmail.com>
2021-10-03 11:43:53 -0400

    added pseudocode for ex28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class Solution28 {
+    public static void main(String[] args) {
+        /*
+        for loop to 5
+            temp = prompt for number
+            total += temp
+        output total statement
+         */
+    }
+}
