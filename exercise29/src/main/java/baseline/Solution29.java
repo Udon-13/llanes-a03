2fdb36048f6541428a77a474cbfdd8ced2d1618c
Udon-13 <maxllanes@gmail.com>
2021-10-03 13:44:39 -0400

    added code for ex29

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -1,11 +1,36 @@
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution29 {
     /*
         prompt for rate
         use hasNextInt to check
         do while loop
-            if hasNextInt
+            if hasNextInt && !=0
                 output statement
      */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str;
+        double temp =0;
+        int hold = 0;
+        do{
+            System.out.print("What is the rate of return? ");
+            if(input.hasNextInt())
+            {
+                temp = input.nextInt();
+                if(temp >0)
+                {
+                    break;
+                }
+            }
+            str = input.nextLine();
+            System.out.println("Sorry. That's not a valid input");
+        }while(hold == 0);
+        temp = 72f/temp;
+        System.out.println("It will take " + (int)temp + " years to double your initial investment.");
+
+    }
+
 }
