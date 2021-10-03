0d1658a9f2a254ebc2d6c10af1cc46bfa0543d04
Udon-13 <maxllanes@gmail.com>
2021-10-03 12:36:11 -0400

    added pseudocode for ex29

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,11 @@
+package baseline;
+
+public class Solution29 {
+    /*
+        prompt for rate
+        use hasNextInt to check
+        do while loop
+            if hasNextInt
+                output statement
+     */
+}
