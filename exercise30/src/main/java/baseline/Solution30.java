d32d42b5e948c9287b3bdd7afd1ef765bfbc6b54
Udon-13 <maxllanes@gmail.com>
2021-10-03 18:01:10 -0400

    added code to ex30

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -6,5 +6,15 @@ public class Solution30 {
         for loop to 12
             output i * j + /t
      */
+    public static void main(String[] args) {
+    for (int i=1;i<=12;i++)
+    {
+        for(int j=1;j<=12;j++)
+            {
+                System.out.print(i*j + "\t");
+            }
+        System.out.print("\n");
+    }
+    }
 
 }
