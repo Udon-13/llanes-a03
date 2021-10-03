40c337b4c94f2777d49f25de4c58aa8cc25ded14
Udon-13 <maxllanes@gmail.com>
2021-10-03 19:20:55 -0400

    added code for ex32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,17 @@
+package baseline;
+
+public class Solution32 {
+    /*
+    method: numberGuessGame
+        do loop while answer != N or n
+            prompt for difficult
+            rand func set to switch case for diff
+            prompt for first answer
+            do loop while guess != randNum
+                if statement for >
+                    too high!
+                else
+                    too low!
+            prompt for playAgain
+     */
+}
