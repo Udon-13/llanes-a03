c4d6310377a31b1271acc95106392c521afbae07
Udon-13 <maxllanes@gmail.com>
2021-10-03 11:52:18 -0400

    added code for ex28

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -1,5 +1,7 @@
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
     public static void main(String[] args) {
         /*
@@ -8,5 +10,16 @@ public class Solution28 {
             total += temp
         output total statement
          */
+        Scanner input = new Scanner(System.in);
+        int total =0;
+        int temp;
+
+        for(int i =0; i<5;i++)
+        {
+            System.out.print("Enter a number: ");
+            temp = input.nextInt();
+            total += temp;
+        }
+        System.out.println("The total is " + total + ".");
     }
 }
