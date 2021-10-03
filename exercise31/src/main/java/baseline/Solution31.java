f6f1148b9baffd5a151ec43ce34f2bc40830a5c1
Udon-13 <maxllanes@gmail.com>
2021-10-03 18:22:36 -0400

    added pseudocode for ex31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,24 @@
+package baseline;
+
+public class Solution31 {
+
+    /*
+    method: targetHeartRate (int age, int restHR)
+        setup statement
+        for loop start at 55 inc ++ 5 stop at 95
+            intense = i / 100
+            int = call formula(age, restHr, intense)
+            output statement
+     */
+
+    /*
+    bool method check (String )
+        hasNextInt loop
+     */
+    public static void main(String[] args) {
+        /*
+        prompt for restPulse and age
+        run through method check
+         */
+    }
+}
