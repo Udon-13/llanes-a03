7d067584e2f778f3c68775d2e07267bc539b67ba
Udon-13 <maxllanes@gmail.com>
2021-10-03 06:21:43 -0400

    added pseudocode for ex27

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,60 @@
+package baseline;
+
+public class Solution27 {
+    /*
+    method: validName (String)
+        if string length is less than 2
+            output statement
+     */
+
+    /*
+    method: validZIP (String)
+        if string != int
+            output statement
+     */
+
+    /*
+    method: validID (String)
+        if length != 7
+            output statement
+        if String[2] != '-'
+            output statement
+        if string 0 & 1 are not letters(capital)
+            output statement
+        if string[3-6] is not number
+            output statement
+
+     */
+
+    /*
+    method: validateInput
+        validName(last) - - - - - - all valid functions return +1 if not valid to count for errors
+        validName(first)
+        validID (id)
+        validZIP (zip)
+     */
+
+    /*
+    method: badOutput (check)
+        switch(check)
+        1 : 2 chars more
+        2 : filled in
+        3 : format!
+        4 : 5 numbers
+     */
+
+    /*
+    method: goodOutput (check)
+        switch(check
+        1 : fName
+        2 : lName
+        3 : zip
+        4 : ID
+        5 : no error
+     */
+    public static void main(String[] args) {
+    /*
+    call valid function
+     */
+    }
+}
