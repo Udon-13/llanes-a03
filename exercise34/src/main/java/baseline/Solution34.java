dace6aca90a1c6f05595498195f04359f053998e
Udon-13 <maxllanes@gmail.com>
2021-10-03 21:18:31 -0400

    added pseudocode for ex34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class Solution34 {
+    /*
+    pre-made array of workers
+    display via for loop
+    ask for what name to delete
+    compare for each element
+    remove same elements from array
+    display array
+     */
+}
