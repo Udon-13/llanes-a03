32f58b3826106953b46d77591d89fd08582688d7
Udon-13 <maxllanes@gmail.com>
2021-10-03 21:45:35 -0400

    added code for ex34

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -1,4 +1,6 @@
 package baseline;
+import java.util.Scanner;
+import java.util.Objects;
 
 public class Solution34 {
     /*
@@ -9,4 +11,39 @@ public class Solution34 {
     remove same elements from array
     display array
      */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] strArray = {"John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"};
+        String[] strArray2 = new String[strArray.length - 1];
+        String str;
+
+        System.out.println("There are " + strArray.length + " employees:");
+        for(int i =0; i<strArray.length;i++)
+        {
+            System.out.println(strArray[i]);
+        }
+        System.out.println();
+        System.out.print("Enter an employee name to remove: ");
+        str = input.nextLine();
+        System.out.println();
+        System.out.println("There are " + strArray2.length + " employees:");
+
+        for (int j=0, k=0;j< strArray.length;j++)
+        {
+            if(Objects.equals(str, strArray[j]))
+            {
+                continue;
+            }
+            else
+            {
+                strArray2[k++] = strArray[j];
+            }
+        }
+
+        for (int l=0;l< strArray2.length;l++)
+        {
+            System.out.println(strArray2[l]);
+        }
+
+    }
 }
